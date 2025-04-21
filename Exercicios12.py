891b648a9e4158230815ffed51419cab41bb0f47
tecIsaias22 <tecnologiaisaias22@gmail.com>
2025-04-21 20:35:26 -0300

    Delete Exercicios12.py

--- Exercicios12.py
+++
@@ -1,6 +0,0 @@
-loja = float(input('Digite o valor da compra: '))
-
-desconto = loja * 0.05
-valor_final = loja - desconto
-
-print('O valor da compra é R${:.2f}, o desconto de 5% é R${:.2f} e o valor final é R${:.2f}'.format(loja, desconto, valor_final))
