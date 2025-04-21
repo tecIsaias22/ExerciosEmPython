253263680f7777e766359fab5aaa54d50bcebf88
tecIsaias22 <tecnologiaisaias22@gmail.com>
2025-04-21 20:35:03 -0300

    Delete Exercicios13.py

--- Exercicios13.py
+++
@@ -1,4 +0,0 @@
-salario = float(input("Digite o salário do funcionário: "))
-aumento = salario * 0.15
-novo_salario = salario + aumento
-print("O novo salário do funcionário é: R${:.2f}".format(novo_salario))
