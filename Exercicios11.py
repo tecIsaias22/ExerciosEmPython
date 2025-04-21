f14653a03d58110ed8d3302ce057b6aa6d47f23c
tecIsaias22 <tecnologiaisaias22@gmail.com>
2025-04-21 20:35:35 -0300

    Delete Exercicios11.py

--- Exercicios11.py
+++
@@ -1,6 +0,0 @@
-pare = float(input("Digite a largura parede: "))
-pared2 = float(input("Digite a altura parede: "))
-area = pare * pared2
-litros = area / 2
-print("A quantidade de tinta necessária para pintar a parede é de {} litros".format(litros))
-
